60270999dc248cb85fb5362589a93ebb36939495
ttnhat17it3 <38066091+ttnhat17it3@users.noreply.github.com>
2018-05-09 15:32:02 +0700

    Bai Tap Cuoi Ky

---
+++ Copy.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap;
+
+/**
+ *
+ * @author Administrator
+ */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Copy {
+    public static void main(String[] args) {
+        // TODO, add your application code
+        try {
+            File fa = new File("C:\\copy.txt");
+            File fb = new File("C:\\paste.txt");            
+            copyFileUsingStream(fa, fb);
+        } catch (IOException io) {
+            System.out.println(io.getMessage());
+        }     
+
+    }
+    
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            is.close();
+            os.close();
+        }
+    }    
+}
+
